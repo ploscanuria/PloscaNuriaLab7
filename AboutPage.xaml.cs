1c7d6c26c8822c928ad87c95aeb2b3b4e4d3e961
NURIA-LUCIA PLOSCA <nuria.plosca@stud.ubbcluj.ro>
2024-11-21 18:33:31 +0200

    Add project files.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace PloscaNuriaLab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+        InitializeComponent(); 
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PloscaNuriaLab7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
