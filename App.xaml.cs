61a925cacc31f47cd63fb9c070d7c70029e4b634
NURIA-LUCIA PLOSCA <nuria.plosca@stud.ubbcluj.ro>
2024-11-26 11:52:58 +0200

    Laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,28 @@
-﻿namespace PloscaNuriaLab7
+﻿using System;
+using PloscaNuriaLab7.Data;
+using System.IO;
+
+
+
+namespace PloscaNuriaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
